5930dd88e77c45782cbfbf11bd08b5c05ca7318f
Adam Peterson <adpete44@gmail.com>
2019-03-24 22:57:58 -0600

    First commit

---
+++ AzureMLAPI/Default.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AzureMLAPI.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>FENIX.AI Client Consulting Matching</title>
+<link rel="stylesheet" runat="server" media="screen" href="~/content/styles.css" />
+
+</head>
+<body>
+
+
+
+
+    <div class="container">  
+    <form id="contact" runat="server">
+    <h3>FENIX.AI Customer Registration Form</h3>
+    <h4>Input Client information to be matched with a consultant</h4>
+    
+        Industry Field
+    <fieldset>
+<asp:DropDownList ID="industryList" runat="server">
+                    <asp:ListItem Value="Education">Education</asp:ListItem>
+                    <asp:ListItem Value="Technology">Technology</asp:ListItem>
+                    <asp:ListItem Value="Retail">Retail</asp:ListItem>
+                    <asp:ListItem Value="Other">Other</asp:ListItem>
+                    </asp:DropDownList>    
+
+    </fieldset>
+    <fieldset>
+                    <asp:TextBox ID="dti" runat="server" placeholder="Project Hours">
+                    </asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Hours are Required" ForeColor="Red" ControlToValidate="dti" Display="Dynamic"></asp:RequiredFieldValidator>
+                    <asp:RangeValidator type="Integer" ID="RangeValidator2" runat="server" ErrorMessage="Number of Hours must be a whole number between 0 to 1000" ForeColor="Red" ControlToValidate="dti" MaximumValue="1000" MinimumValue="0" Display="Dynamic"></asp:RangeValidator>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Special Comments or Requests (optional)" type="tel" tabindex="3">
+    </fieldset>
+    <fieldset>
+      <input placeholder="Client Referral Code (optional)" type="url" tabindex="4">
+    </fieldset>
+   
+    <fieldset>
+        <asp:LinkButton ID="lbPredict" type="submit" runat="server" OnClick="lbPredict_Click">Match</asp:LinkButton>
+    </fieldset>
+                <asp:Label ID="lblResults" runat="server" Font-Size="Large"></asp:Label>
+
+  </form>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ AzureMLAPI/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace AzureMLAPI
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //var client = new RestClient("https://ussouthcentral.services.azureml.net/workspaces/9d5c479f32b0474c9cd569ba02e4411c/services/e160e605c3b14d8f8a57d1860a0a3e5f/execute?api-version=2.0&details=true");
+            //var request = new RestRequest(Method.POST);
+           // request.AddHeader("Postman-Token", "b4df259c-8b4d-48a7-86bd-0276a127b823");
+           // request.AddHeader("cache-control", "no-cache");
+            //request.AddHeader("Content-Type", "application/json");
+            //request.AddHeader("Authorization", "Bearer fSj9ISeXrDIfaie8RjSciJnfBnCYGixYWrkQVLzpjmgiDut1tr3ZP+g9qYVAvzxXlcpicCGchcOSWq3W2NAP4w==");
+           // request.AddParameter("undefined", "{\r\n  \"Inputs\": {\r\n    \"input1\": {\r\n      \"ColumnNames\": [\r\n        \"ClientID\",\r\n        \"Client\",\r\n        \"Needed Hours\",\r\n        \"Industry\",\r\n        \"Rating\",\r\n        \"Consultant\"\r\n      ],\r\n      \"Values\": [\r\n        [\r\n          \"1\",\r\n          \"Client1\",\r\n          \"" + dti.Text + "\",\r\n          \"" + industryList.SelectedValue + "\",\r\n          \"0\",\r\n          \"1\"\r\n        ]\r\n      ]\r\n    }\r\n  },\r\n  \"GlobalParameters\": {}\r\n}", ParameterType.RequestBody);
+           // IRestResponse response = client.Execute(request);
+            //lblResults.Text = response.Content.ToString();
+        }
+
+        protected void lbPredict_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://ussouthcentral.services.azureml.net/workspaces/9d5c479f32b0474c9cd569ba02e4411c/services/e160e605c3b14d8f8a57d1860a0a3e5f/execute?api-version=2.0&details=true");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Postman-Token", "b4df259c-8b4d-48a7-86bd-0276a127b823");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Bearer fSj9ISeXrDIfaie8RjSciJnfBnCYGixYWrkQVLzpjmgiDut1tr3ZP+g9qYVAvzxXlcpicCGchcOSWq3W2NAP4w==");
+            request.AddParameter("undefined", "{\r\n  \"Inputs\": {\r\n    \"input1\": {\r\n      \"ColumnNames\": [\r\n        \"ClientID\",\r\n        \"Client\",\r\n        \"Needed Hours\",\r\n        \"Industry\",\r\n        \"Rating\",\r\n        \"Consultant\"\r\n      ],\r\n      \"Values\": [\r\n        [\r\n          \"1\",\r\n          \"Client1\",\r\n          \"" + dti.Text + "\",\r\n          \"" + industryList.SelectedValue + "\",\r\n          \"0\",\r\n          \"1\"\r\n        ]\r\n      ]\r\n    }\r\n  },\r\n  \"GlobalParameters\": {}\r\n}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            //lblResults.Text = response.Content.ToString();
+
+            var results = JObject.Parse(response.Content);
+            string prediction = results["Results"]["output1"]["value"]["Values"].ToString();
+            prediction = prediction.Replace("[", "").Replace("]", "").Replace("\"", "");
+
+           lblResults.Text = (prediction);
+        }
+    }
+}
